c3919fa5ed0a8beca22c5e14931c3ba0126b969a
antos07 <trotsenko.ant07@gmail.com>
2023-03-03 00:21:42 +0100

    Fix typo in env var name

--- main-back/main_back/settings.py
+++ main-back/main_back/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ['DJANGO_DEBUH']))
+DEBUG = bool(int(os.environ['DJANGO_DEBUG']))
 
 ALLOWED_HOSTS = []
 
