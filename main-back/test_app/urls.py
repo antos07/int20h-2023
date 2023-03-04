e3298196410d6fbe7eb59da391d9e70c4426258b
antos07 <trotsenko.ant07@gmail.com>
2023-03-04 16:47:18 +0100

    Remove test_app app

--- main-back/test_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from test_app import views
-
-app_name = 'test_app'
-
-urlpatterns = [
-    path('', views.HelloWorldView.as_view()),
-]
--- main-back/test_app/views.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class HelloWorldView(TemplateView):
-    template_name = 'test_app/hello_world.html'
