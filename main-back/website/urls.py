4b6cd013b17219a56821ae0f23f5c5895d8f5310
antos07 <trotsenko.ant07@gmail.com>
2023-03-04 22:45:51 +0100

    Add user projects view

--- main-back/website/urls.py
+++ main-back/website/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
 
-from website.views import RegisterView, ProfileView
+from website.views import RegisterView, ProfileView, UserProjectsView
 
 app_name = 'website'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='test_app/login.html'), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
+    path('profile/<int:user_id>/projects', UserProjectsView.as_view(), name='user_projects'),
 ]
--- main-back/website/views.py
+++ main-back/website/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views import generic
 
 from website.forms import RegistrationForm
+from website.models import Project, Participation, Invitation, JoinRequest
 
 
 class RegisterView(generic.CreateView):
@@ -17,3 +19,30 @@ class ProfileView(generic.DetailView):
     model = User
     context_object_name = 'showed_user'
     template_name = 'test_app/profile.html'
+
+
+class UserProjectsView(generic.ListView):
+    template_name = 'test_app/user_projects.html'
+    context_object_name = 'projects'
+    requested_user: User
+
+    def dispatch(self, request, *args, **kwargs):
+        self.requested_user = get_object_or_404(User, pk=kwargs['user_id'])
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        return Participation.objects.filter(user=self.requested_user).order_by('-since')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context_data = super().get_context_data(object_list=object_list, **kwargs)
+        context_data['requested_user'] = self.requested_user
+        context_data['pending_invitations'] = []
+        context_data['pending_join_requests'] = []
+        if self.requested_user == self.request.user:
+            context_data['pending_invitations'] = (Invitation.objects
+                                                   .filter(user=self.requested_user, status=Invitation.PENDING)
+                                                   .order_by('-timestamp'))
+            context_data['pending_join_requests'] = (JoinRequest.objects
+                                                     .filter(user=self.requested_user, status=JoinRequest.PENDING)
+                                                     .order_by('-timestamp'))
+        return context_data
