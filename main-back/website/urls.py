8070f6ef62edcbc196c44fc48593264e3caa2b22
antos07 <trotsenko.ant07@gmail.com>
2023-03-04 18:53:31 +0100

    Add login view

--- main-back/website/urls.py
+++ main-back/website/urls.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
 app_name = 'website'
 
-urlpatterns = []
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='test_app/login.html'), name='login'),
+]
