56e2681e6081ed18241e886ab93af9a8ece4ebc3
antos07 <trotsenko.ant07@gmail.com>
2023-03-04 18:04:50 +0100

    Add models

--- main-back/website/admin.py
+++ main-back/website/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+admin.site.register(models.Profile)
+admin.site.register(models.Project)
+admin.site.register(models.Role)
+admin.site.register(models.Skill)
+admin.site.register(models.Participation)
--- main-back/website/models.py
+++ main-back/website/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    about = models.TextField()
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    telegram = models.CharField(max_length=32, null=True, blank=True)
+    linkedin = models.URLField(null=True, blank=True)
+    skills = models.ManyToManyField(Skill)
+    is_mentor = models.BooleanField(default=False)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    is_active = models.BooleanField(default=True)
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    skills = models.ManyToManyField(Skill)
+    project = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    is_active = models.BooleanField(default=True)
+
+
+class Participation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    role = models.ForeignKey(Role, on_delete=models.DO_NOTHING)
+    since = models.DateField()
+    to = models.DateField(null=True)
