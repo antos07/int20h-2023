5105465542c1df7d25e15a281a899695eb6b063d
antos07 <trotsenko.ant07@gmail.com>
2023-03-05 01:20:30 +0100

    Update urlpatterns to be more logical

--- main-back/website/urls.py
+++ main-back/website/urls.py
@@ -8,8 +8,8 @@ app_name = 'website'
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='website/login.html'), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
-    path('profile/<int:user_id>/projects', UserProjectsView.as_view(), name='user_projects'),
+    path('users/<int:pk>', ProfileView.as_view(), name='profile'),
+    path('users/<int:user_id>/projects', UserProjectsView.as_view(), name='user_projects'),
     path('projects/', ProjectListView.as_view(), name='projects'),
     path('users/', UserListView.as_view(), name='users'),
 ]
