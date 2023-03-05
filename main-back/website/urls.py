b8c94b59c5925b101300f2dc0f4cfe034523c7ab
antos07 <trotsenko.ant07@gmail.com>
2023-03-05 01:30:45 +0100

    Implement logout

--- main-back/website/urls.py
+++ main-back/website/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from django.views.generic import RedirectView
 
@@ -9,9 +9,10 @@ app_name = 'website'
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='website/login.html'), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('users/<int:pk>', ProfileView.as_view(), name='profile'),
     path('users/<int:user_id>/projects', UserProjectsView.as_view(), name='user_projects'),
     path('projects/', ProjectListView.as_view(), name='projects'),
     path('users/', UserListView.as_view(), name='users'),
-    path('', RedirectView.as_view(pattern_name='website:projects'))
+    path('', RedirectView.as_view(pattern_name='website:projects'), name='home')
 ]
